f8fb5e13a2d2a669f900a5eaf2b463812f45a575
pvillaverde <pvillaverde@qualigy.com>
2022-12-18 17:30:42 +0100

    [Feat] Full RSS Feed - Asociados na landing

--- src/blog/feed.tmpl.js
+++ src/blog/feed.tmpl.js
@@ -18,7 +18,7 @@ export default function (
          id: url(post.data.url, true),
          url: url(post.data.url, true),
          title: post.data.title,
-         content_html: htmlUrl(md(post.data.excerpt || post.data.content), true),
+         content_html: htmlUrl(md(post.data.content || post.data.excerpt), true),
          date_published: date(post.data.date, "ATOM"),
       });
    }
