b071e22824363eb361c0ea61ecaa46b404f77ec4
pvillaverde <pvillaverde@qualigy.com>
2023-08-18 21:14:53 +0200

    Add active source

---
+++ src/api/tags_active.js
@@ -0,0 +1,16 @@
+export const url = "/api/tags_active.json";
+export default function ({ search, categorias }) {
+   const items = [];
+   for (const tag of categorias.tags) {
+      const pagesByTag = search.pages(`type=proxecto active=true ${tag.id}`, "title=asc");
+      items.push({
+         etiqueta_id: tag.id,
+         etiqueta_nome: tag.name,
+         total: pagesByTag.length,
+         proxectos: pagesByTag.map(p => p.data.title)
+      });
+   }
+   items.sort((a, b) => a.total - b.total);
+
+   return JSON.stringify(items, null, 2);
+}
